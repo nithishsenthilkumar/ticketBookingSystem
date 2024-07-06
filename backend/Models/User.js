3ede921d1e0188a44cf593e5fec16d8c090489a0
nithishsenthilkumar <727721euec105@skcet.ac.in>
2024-07-06 21:26:02 +0530

    "nithish"

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,31 +1,36 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true},
-  email: {type: String, required: true, unique: true},
-  phonenumber: {type: Number, required: true, unique: true,
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  phonenumber: {
+    type: Number,
+    required: true,
+    unique: true,
     validate: {
       validator: function (value) {
         return value.toString().length == 10;
       },
       message: "Please check the Mobile Number",
-    }
+    },
   },
-  password: {type: String, required: true,
+  password: {
+    type: String,
+    required: true,
     validate: {
       validator: function (value) {
         return value.toString().length >= 8;
       },
       message: "Password must be at least 8 characters long",
-    }
+    },
   },
   address: {
     street: { type: String },
     city: { type: String },
     state: { type: String },
-    country: { type: String }
+    country: { type: String },
   },
-  created_at: {type: Date, default: Date.now},
+  created_at: { type: Date, default: Date.now },
 });
 
 const User = mongoose.model("User", userSchema);
