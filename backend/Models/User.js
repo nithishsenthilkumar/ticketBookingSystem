c420429a86b1d30005aeda307ba53d39ec96ea1a
nithishsenthilkumar <727721euec105@skcet.ac.in>
2024-07-06 21:25:07 +0530

    "nithish"

--- backend/Models/User.js
+++ backend/Models/User.js
@@ -1,37 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username:{
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email:{
-        type: String,
-        require: true,
-        unique: true,
-    },
-    phonenumber:{
-        type: Number,
-        required: true,
-        unique: true,
-        validate: {
-            validator: function(value){
-                return value.toString().length == 10;
-            },
-            message: "Please check the Mobile Number",
-        },
-    },
-    password:{
-        type: String,
-        required: true,
-        validate: {
-            validator: function(value){
-                return value.toString().length >= 8;
-            },
-            message: "Password must be at least 8 characters long",
-        },
-    },
+  username: {type: String, required: true, unique: true},
+  email: {type: String, required: true, unique: true},
+  phonenumber: {type: Number, required: true, unique: true,
+    validate: {
+      validator: function (value) {
+        return value.toString().length == 10;
+      },
+      message: "Please check the Mobile Number",
+    }
+  },
+  password: {type: String, required: true,
+    validate: {
+      validator: function (value) {
+        return value.toString().length >= 8;
+      },
+      message: "Password must be at least 8 characters long",
+    }
+  },
+  address: {
+    street: { type: String },
+    city: { type: String },
+    state: { type: String },
+    country: { type: String }
+  },
+  created_at: {type: Date, default: Date.now},
 });
 
 const User = mongoose.model("User", userSchema);
