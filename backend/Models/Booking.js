137f2f697f1f94c70c653e19e9135daf0b0b9c78
nithishsenthilkumar <727721euec105@skcet.ac.in>
2024-07-06 23:13:55 +0530

    "nithish"

--- backend/Models/Booking.js
+++ backend/Models/Booking.js
@@ -1,5 +1,13 @@
 const mongoose = require('mongoose');
 
 const bookingSchema = new mongoose.Schema({
-    
-})
+    userId: {type: Schema.Types.ObjectId, ref: 'User', require: true},
+    bookedData: {type: Date, default: Date.now, required: true},
+    status: {type: String},
+    booked_at: {type: Date, default: Date.now},
+    totalAmount: {type: Number, required: true}
+});
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
--- backend/Models/Theatre.js
+++ backend/Models/Theatre.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
+const seatSchema = new mongoose.Schema({
+    seatId: {type: String, required: true},
+    isFilled: {type: Boolean, default: false}
+});
+
+const showTimingSchema = new mongoose.Schema({
+    showTime: {type: Date, required: true},
+    seats: { type: seatSchema }
+});
+
 const theatreSchema = new mongoose.Schema({
-    id:{type: Number, required: true, unique: true},
-    name:{type: String, required: true},
-    location:{type: Location, required: true},
+    id: {type: Number, required: true, unique: true},
+    name: {type: String, required: true},
+    location: {type: Location, required: true},
+    showTiming: {type: showTimingSchema}
 });
 
 const Theatre = mongoose.model("Theatre", theatreSchema);
