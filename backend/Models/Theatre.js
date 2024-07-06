5221d2cf39b7460d940f48f2b5c2475d13f33d10
nithishsenthilkumar <727721euec105@skcet.ac.in>
2024-07-06 20:47:35 +0530

    "nithish"

---
+++ backend/Models/Theatre.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const theatreSchema = new mongoose.Schema({
+    id:{
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    name:{
+        type: String, 
+        required: true,
+    },
+    location:{
+        type: Location,
+        required: true,
+    },
+});
+
+const Theatre = mongoose.model("Theatre", theatreSchema);
+
+module.exports = Theatre;
