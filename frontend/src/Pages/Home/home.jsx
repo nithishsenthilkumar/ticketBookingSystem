508447f890de4e9d52a8efbd8c464ea6e40700d2
nithishsenthilkumar <727721euec105@skcet.ac.in>
2025-01-23 19:48:36 +0530

    initial commit

--- frontend/src/Pages/Home/home.jsx
+++ frontend/src/Pages/Home/home.jsx
@@ -1,22 +1,71 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import MovieCard from "../../Components/Cards/MovieCard";
 
 const Home = () => {
+  const movies = [
+    {
+      id: 1,
+      title: "Movie 1",
+      rating: 4.5,
+      description: "Description of Movie 1",
+    },
+    {
+      id: 2,
+      title: "Movie 2",
+      rating: 3.8,
+      description: "Description of Movie 2",
+    },
+    {
+      id: 1,
+      title: "Movie 1",
+      rating: 4.5,
+      description: "Description of Movie 1",
+    },
+    {
+      id: 2,
+      title: "Movie 2",
+      rating: 3.8,
+      description: "Description of Movie 2",
+    },
+    {
+      id: 1,
+      title: "Movie 1",
+      rating: 4.5,
+      description: "Description of Movie 1",
+    },
+    {
+      id: 2,
+      title: "Movie 2",
+      rating: 3.8,
+      description: "Description of Movie 2",
+    },
+    {
+      id: 1,
+      title: "Movie 1",
+      rating: 4.5,
+      description: "Description of Movie 1",
+    },
+    {
+      id: 2,
+      title: "Movie 2",
+      rating: 3.8,
+      description: "Description of Movie 2",
+    },
+  ];
+
   return (
-    <div className="h-screen">
-      <div className="flex items-center justify-between p-4">
-        <div className="w-20">
-          <img src={logo} alt="logo" className="w-full h-auto" />
-        </div>
-        <div className="relative w-full max-w-md mx-4">
+    <div className="container-fluid bg-white">
+      <div className="fixed w-full grid bg-white justify-items-end">
+        <div className="top-0 right-0 w-1/5 p-4 rounded-lg flex items-center pt-4">
           <input
             type="text"
             placeholder="Search..."
-            className="w-full p-4 pr-12 text-sm border rounded-lg shadow-sm focus:border-blue-500 focus:ring-blue-500"
+            className="w-full bg-black p-2 rounded-lg"
           />
-          <button className="absolute inset-y-0 right-0 flex items-center pr-4 text-blue-500">
+          <button className="flex items-center justify-center bg-black text-white rounded-lg p-2 ml-2">
             <svg
-              className="w-5 h-5"
+              className="w-6 h-6"
               fill="none"
               stroke="currentColor"
               viewBox="0 0 24 24"
@@ -31,24 +80,16 @@ const Home = () => {
             </svg>
           </button>
         </div>
-        <div>
-          <button className="focus:outline-none">
-            <svg
-              className="w-8 h-8 text-gray-800"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
-          </button>
-        </div>
+      </div>
+      <div className="flex flex-wrap justify-center px-4 pt-16">
+        {movies.map((movie) => (
+          <MovieCard
+            key={movie.id}
+            title={movie.title}
+            rating={movie.rating}
+            description={movie.description}
+          />
+        ))}
       </div>
     </div>
   );
--- frontend/src/Pages/Signup/Signup.jsx
+++ frontend/src/Pages/Signup/Signup.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axiosInstance from "../../utils/axiosInstance";
 import side from "../../assets/side.jpg";
+import { ReactLenis, useLenis } from "../../../libs/lenis";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
@@ -62,118 +63,128 @@ const Signup = () => {
     }
   };
 
-  return (
-    <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center bg-cover bg-no-repeat">
-      <div className="absolute h-screen inset-0 bg-black opacity-40"></div>
-      <div className="w-full max-w-4xl flex p-12">
-        <div className="hidden md:flex w-full">
-          <img
-            src={side}
-            alt="side"
-            className="border-white border-l-8 rounded-l-full object-cover w-full"
-          />
+  function Layout() {
+    const lenis = useLenis(({ scroll }) => {});
+
+    return (
+      <ReactLenis root>
+        <div className="h-screen bg-gradient-to-r from-cyan-500 to-blue-500 flex items-center justify-center bg-cover bg-no-repeat">
+          <div className="absolute h-screen inset-0 bg-black opacity-40"></div>
+          <div className="w-full max-w-4xl flex p-12">
+            <div className="hidden md:flex w-full">
+              <img
+                src={side}
+                alt="side"
+                className="border-white border-l-8 rounded-l-full object-cover w-full"
+              />
+            </div>
+            <div className="relative bg-white p-12 shadow-md w-full max-w-md rounded-lg md:rounded-r-lg md:rounded-none">
+              <h2 className="text-2xl font-head font-bold tracking-wider font-title mb-6 text-center">
+                CREATE PROFILE
+              </h2>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="username"
+                    value={formData.username}
+                    placeholder="username"
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="email"
+                    value={formData.email}
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="phonenumber"
+                    placeholder="phone number"
+                    value={formData.phonenumber}
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                <div className="mb-6">
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="password"
+                    value={formData.password}
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="address.street"
+                    placeholder="street"
+                    value={formData.address.street}
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="address.city"
+                    placeholder="city"
+                    value={formData.address.city}
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="address.state"
+                    placeholder="state"
+                    value={formData.address.state}
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                <div className="mb-4">
+                  <input
+                    type="text"
+                    name="address.country"
+                    placeholder="country"
+                    value={formData.address.country}
+                    onChange={handleChange}
+                    className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
+                    required
+                  />
+                </div>
+                {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
+                <button
+                  type="submit"
+                  className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-200"
+                >
+                  Sign Up
+                </button>
+              </form>
+            </div>
+          </div>
         </div>
-        <div className="relative bg-white p-12 shadow-md w-full max-w-md rounded-lg md:rounded-r-lg md:rounded-none">
-          <h2 className="text-2xl font-head font-bold tracking-wider font-title mb-6 text-center">
-            CREATE PROFILE
-          </h2>
-          <form onSubmit={handleSubmit}>
-            <div className="mb-4">
-              <input
-                type="text"
-                name="username"
-                value={formData.username}
-                placeholder="username"
-                onChange={handleChange}
-                className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="email"
-                name="email"
-                placeholder="email"
-                value={formData.email}
-                onChange={handleChange}
-                className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                name="phonenumber"
-                placeholder="phone number"
-                value={formData.phonenumber}
-                onChange={handleChange}
-                className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-6">
-              <input
-                type="password"
-                name="password"
-                placeholder="password"
-                value={formData.password}
-                onChange={handleChange}
-                className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                name="address.street"
-                placeholder="street"
-                value={formData.address.street}
-                onChange={handleChange}
-                className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                name="address.city"
-                placeholder="city"
-                value={formData.address.city}
-                onChange={handleChange}className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                name="address.state"
-                placeholder="state"
-                value={formData.address.state}
-                onChange={handleChange}className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <input
-                type="text"
-                name="address.country"
-                placeholder="country"
-                value={formData.address.country}onChange={handleChange}className="w-full text-black font-semibold tracking-wide text-base border-b-2 border-inherit p-2 rounded focus:outline-none"
-                required
-              />
-            </div>
-            {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
-            <button
-              type="submit"
-              className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600 transition duration-200"
-            >
-              Sign Up
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  );
+      </ReactLenis>
+    );
+  }
 };
 
 export default Signup;
